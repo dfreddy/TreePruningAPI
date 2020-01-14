283559d169c62e9026f0e7779faef67d3f60a4ad
Frederico Rocha <up201408030@fe.up.pt>
2020-01-14 19:12:44 +0000

    Pruning algorithm

--- api.py
+++ api.py
@@ -19,16 +19,56 @@ def getTree(name):
 
     data = response.json()
 
-    ''' testing json response
+    # testing json response
     output_file = open("output.txt","w+")
-    json.dump(data, output_file)
+    json.dump(data, output_file, indent=4)
     output_file.close()
-    '''
 
     return data
 
+# iterates over the original tree
+# removes indicators that werent selected
+# cleans out categories that have no indicators
+# same for sub_themes and themes
 def pruneTree(tree, indicator_ids):
-    return 0
+    themes_list = []
+
+    for a in tree:
+        sub_themes_list = []
+
+        for b in a['sub_themes']:
+            categories_list = []
+
+            for c in b['categories']:
+                indicators_list = []
+
+                for d in c['indicators']:
+                    if str(d['id']) in indicator_ids:
+                        indicators_list.append(d)
+
+                # if category c has no indicators, removes it
+                c['indicators'] = indicators_list
+                if c['indicators'] != []:
+                    categories_list.append(c)
+
+            # if sub theme b has no categories, removes it
+            b['categories'] = categories_list
+            if b['categories'] != []:
+                sub_themes_list.append(b)
+
+        # if theme a has no sub themes, removes it
+        a['sub_themes'] = sub_themes_list
+        if a['sub_themes'] != []:
+            themes_list.append(a)
+
+    tree = themes_list
+
+    # testing pruned output
+    output_file = open("pruned_output.txt","w+")
+    json.dump(tree, output_file, indent=4)
+    output_file.close()
+
+    return tree
 
 @app.route('/tree/<string:name>', methods=['GET'])
 def pruner(name):
@@ -38,7 +78,7 @@ def pruner(name):
 
     pruned_tree = pruneTree(tree, indicator_ids)
 
-    return jsonify(tree)
+    return jsonify(pruned_tree)
 
 '''
 @app.route('/tree/<string:name>', methods=['GET'])
